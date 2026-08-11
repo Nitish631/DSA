463ea1b0842efa44cf3255b1019bdcb988982c41
Nitish631 <nitishrajbanshi61@gmail.com>
2026-08-12 00:33:57 +0545

    Conditional Statement Started

---
+++ 0003ConditionalStateLoops.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=44;
+    if(n>=0){
+        cout<<"Positive"<<endl;
+    }else{
+        cout<<"Negative"<<endl;
+    }
+    if(n%2==0){
+        cout<<"EVEN"<<endl;
+    }else{
+        cout<<"ODD"<<endl;
+    }
+    return 0;
+}
