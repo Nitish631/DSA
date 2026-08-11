f4322860fe943a820417dd405eb8b71f84987589
Nitish631 <nitishrajbanshi61@gmail.com>
2026-08-12 01:45:52 +0545

    Lecture 3 completed

--- 0003ConditionalStateLoops.cpp
+++ 0003ConditionalStateLoops.cpp
@@ -42,7 +42,60 @@ int main(){
         cout<<count<<" ";
         count++;
     }
+    //// FOR LOOP
+    for(int i=1;i<=200;i+=2){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    //// Sum of numbers from 1 to n
+    int sum=0;
+    n=55;
+    for(int i =1;i<=n;i++){
+        sum+=i;
+    }
+    cout<<"SUM IS : "<<sum<<endl;
+    //// BREAK AND CONTINUE
+    for(int i =1;i<=50;i++){
+        cout<<i<<" ";
+        if(i==5){break;}
+    }
+    cout<<"\n";
+    for(int i =1 ; i<=10;i++){
+        if(i==5){
+            continue;
+        }
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    //// DO WHILE LOOP
+    n=0;
+    do{
+        cout<<"\n Hello world!";
+        n++;
+    }while(n<=10);
+    cout<<"\n";
 
+    //// CHECK IF PRIME OR NOT
+    n=21;
+    bool isPrime=true;
+    for(int i=2;i*i<=n;i++){
+        if (n%i==0){
+            isPrime=false;
+            break;
+        }
+    }
+    if(isPrime){
+        cout<<"PRIME"<<endl;
+    }else{
+        cout<<"COMPOSITE"<<endl;
+    }
+    ///// NESTED LOOP
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<"\n";
+    }
     
     return 0;
 }
