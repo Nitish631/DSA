e777e72a8eb53e4e7e477f0526e647af555b1ea1
Nitish631 <nitishrajbanshi61@gmail.com>
2026-08-19 03:24:25 +0545

    binary to decimal function is added

--- 0006BinaryNS.cpp
+++ 0006BinaryNS.cpp
@@ -11,7 +11,19 @@ int decimalToBinary(int n){
     }
     return binNum;
 }
+int binaryToDecimal(int b){
+    int pow=1;
+    int dec=0;
+    while(b>0){
+        int rem=b%10;
+        dec+=rem*pow;
+        pow*=2;
+        b/=10;
+    }
+    return dec;
+}
 int main(){
+    ///DECIMAL TO BINARY
     cout<<"Enter a number ";
     int n;
     cin>>n;
@@ -19,5 +31,12 @@ int main(){
         int b=decimalToBinary(i);
         cout<<"Decimal "<<i<<" = "<<b<<"\n";
     }
+
+    ///BINARY TO DECIMAL
+    cout<<"Enter the binary number: ";
+    int b;
+    cin>>b;
+    int dec=binaryToDecimal(b);
+    cout<<"Decimal of "<<b<<" is "<<dec<<"\n";
     return 0;
 }
